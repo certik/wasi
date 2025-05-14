dee7e8e6117735fbf4728fa19a439891c736b0d4
Ondřej Čertík <ondrej@certik.us>
2025-05-14 10:25:52 -0600

    Show import/export

--- build.sh
+++ build.sh
@@ -13,6 +13,9 @@ $CLANG \
     -o example.wasm \
     example.c
 
+wasm-objdump -x -j Import example.wasm
+wasm-objdump -x -j Export example.wasm
+
 wasmtime run --invoke mysin example.wasm 1.5
 wasmtime run --invoke add example.wasm 5 7
 node example.js
