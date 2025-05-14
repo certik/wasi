ca5ae1bfd13d1bdda507cc4963b6ef81b23d5541
Ondřej Čertík <ondrej@certik.us>
2025-05-14 09:43:40 -0600

    Use C standard library

--- build.sh
+++ build.sh
@@ -8,10 +8,11 @@ CLANG=$WASI_SDK/bin/clang
 $CLANG \
     -O2 -s \
     -Wl,--export=add \
-    -nostdlib \
+    -Wl,--export=mysin \
     -Wl,--no-entry \
     -o example.wasm \
     example.c
 
-wasmtime example.wasm
+wasmtime run --invoke mysin example.wasm 1.5
+wasmtime run --invoke add example.wasm 5 7
 node example.js
--- example.c
+++ example.c
@@ -1,4 +1,10 @@
+#include <math.h>
+
 // Export the add function for JavaScript and Wasmtime
 int add(int a, int b) {
     return a + b;
+}
+
+double mysin(double a) {
+    return sin(a);
 }
