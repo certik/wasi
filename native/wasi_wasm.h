6bfa7820cf8a9950fd26106e27328bb2a26da1c9
Ondřej Čertík <ondrej@certik.us>
2025-08-05 12:28:39 -0700

    Simplify wasi_wasm.h

--- native/wasi_wasm.h
+++ native/wasi_wasm.h
@@ -1,15 +1,9 @@
 #include <stdlib.h>
 #include <wasi.h>
 
-__attribute__((
-    __import_module__("wasi_snapshot_preview1"),
-    __import_name__("fd_write")
-))
-uint32_t fd_write(int fd, const ciovec_t* iovs, size_t iovs_len, size_t* nwritten);
+#define WASI(name) __attribute__((__import_module__("wasi_snapshot_preview1"), __import_name__(#name))) name
 
+uint32_t WASI(fd_write)(int fd, const ciovec_t* iovs, size_t iovs_len, size_t* nwritten);
+void WASI(proc_exit)(int status);
 
-__attribute__((
-    __import_module__("wasi_snapshot_preview1"),
-    __import_name__("proc_exit")
-))
-void proc_exit(int status);
+#undef WASI
