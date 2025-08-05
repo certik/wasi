7f78d261498b28560656ecd408274e183b1cbbda
Ondřej Čertík <ondrej@certik.us>
2025-08-05 11:16:58 -0600

    Create base_io.h/c

---
+++ base/base_io.c
@@ -0,0 +1,27 @@
+#include <base_io.h>
+
+uint32_t write_all(int fd, ciovec_t* iovs, size_t iovs_len) {
+    size_t i;
+    size_t nwritten;
+    uint32_t ret;
+
+    for (i = 0; i < iovs_len; ) {
+        ret = fd_write(fd, &iovs[i], iovs_len - i, &nwritten);
+        if (ret != 0) {
+            return ret; // Return error code
+        }
+
+        // Advance through the iovecs based on how much was written
+        while (nwritten > 0 && i < iovs_len) {
+            if (nwritten >= iovs[i].buf_len) {
+                nwritten -= iovs[i].buf_len;
+                i++;
+            } else {
+                iovs[i].buf = (const uint8_t*)iovs[i].buf + nwritten;
+                iovs[i].buf_len -= nwritten;
+                nwritten = 0;
+            }
+        }
+    }
+    return 0; // Success
+}
