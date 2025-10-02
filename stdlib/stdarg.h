92fdf9c8ebc55979f313b11eaf1a7c1b584448b2
Ondřej Čertík <ondrej@certik.us>
2025-10-02 09:37:45 -0600

    Update

--- stdlib/stdarg.h
+++ stdlib/stdarg.h
@@ -10,9 +10,9 @@ typedef char* va_list;
 #if defined(_M_X64) || defined(_M_ARM64)
 // x64 and ARM64 use different calling conventions
 // MSVC provides __crt_va_start, __crt_va_arg, __crt_va_end as intrinsics
-#define va_start __crt_va_start
-#define va_arg __crt_va_arg
-#define va_end __crt_va_end
+#define va_start(ap, v) __crt_va_start(&(ap), &(v))
+#define va_arg(ap, t) (*(t*)(__crt_va_arg(&(ap), sizeof(t))))
+#define va_end(ap) __crt_va_end(&(ap))
 #define va_copy(dest, src) ((dest) = (src))
 #else
 // x86 (32-bit) fallback
