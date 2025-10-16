0bb621b1da20d01abc90c05193d9d831d1b073cd
Ondřej Čertík <ondrej@certik.us>
2025-10-15 20:51:02 -0600

    Refactor tests

---
+++ test_base.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// Individual test functions
+void test_wasi_heap(void);
+void test_buddy(void);
+void test_arena(void);
+void test_scratch(void);
+void test_format(void);
+void test_io(void);
+void test_hashtable_int_string(void);
+void test_hashtable_string_int(void);
+void test_vector_int(void);
+void test_vector_int_ptr(void);
+
+// Main test runner
+void test_base(void);
