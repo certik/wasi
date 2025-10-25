54381dcc4e3170422584fa6cbd033000c3407459
Ondřej Čertík <ondrej@certik.us>
2025-10-25 14:15:30 -0600

    Draw something

--- gm_sdl.c
+++ gm_sdl.c
@@ -1,5 +1,6 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <stdbool.h>
 
 int main(int argc, char* args[]) {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -7,17 +8,56 @@ int main(int argc, char* args[]) {
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Hello SDL 3", 640, 480, 0);
+    SDL_Window* window = SDL_CreateWindow("Mouse Draw SDL 3", 640, 480, 0);
     if (window == NULL) {
         SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
 
-    SDL_PumpEvents();  // Force window to display on macOS
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    if (renderer == NULL) {
+        SDL_Log("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
 
-    SDL_Delay(3000);  // Wait 3 seconds
+    bool quit = false;
+    SDL_Event event;
+    int mouse_x = 0, mouse_y = 0;
 
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    quit = true;
+                    break;
+                case SDL_EVENT_KEY_DOWN:
+                    if (event.key.key == SDLK_Q) {
+                        quit = true;
+                    }
+                    break;
+                case SDL_EVENT_MOUSE_MOTION:
+                    mouse_x = event.motion.x;
+                    mouse_y = event.motion.y;
+                    break;
+            }
+        }
+
+        // Clear screen to black
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Draw a red "blob" (filled rectangle) under the mouse cursor
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_FRect blob = { .x = (float)mouse_x - 10, .y = (float)mouse_y - 10, .w = 20, .h = 20 };
+        SDL_RenderFillRect(renderer, &blob);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
