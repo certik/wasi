72e3183b1fb7761c15a6bc1098e4dcfcf55968b3
Ondřej Čertík <ondrej@certik.us>
2025-11-01 21:46:46 -0600

    linux fix

--- base/base_types.h
+++ base/base_types.h
@@ -1,33 +1,26 @@
 #pragma once
 
-// Basic integer types - guard against conflicts with standard headers
-#ifndef __uint8_t_defined
+// When building with SDL (which uses system headers), use standard headers
+#if defined(WASI_LINUX_SKIP_ENTRY) || defined(WASI_MACOS_SKIP_ENTRY) || defined(WASI_WINDOWS_SKIP_ENTRY)
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#else
+// Basic integer types for nostdlib builds
 typedef unsigned char uint8_t;
-#endif
-#ifndef __uint16_t_defined
 typedef unsigned short uint16_t;
-#endif
-#ifndef __uint32_t_defined
 typedef unsigned int uint32_t;
-#endif
-#ifndef __uint64_t_defined
 typedef unsigned long long uint64_t;
-#endif
 
-#ifndef __int8_t_defined
 typedef signed char int8_t;
-#endif
-#ifndef __int16_t_defined
 typedef signed short int16_t;
-#endif
-#ifndef __int32_t_defined
 typedef signed int int32_t;
-#endif
-#ifndef __int64_t_defined
 typedef signed long long int64_t;
 #endif
 
-// Pointer-sized integer type
+// Pointer-sized integer type and size types (only for nostdlib builds)
+#if !defined(WASI_LINUX_SKIP_ENTRY) && !defined(WASI_MACOS_SKIP_ENTRY) && !defined(WASI_WINDOWS_SKIP_ENTRY)
+
 #if defined(_WIN32) && defined(_WIN64)
     // For 64 bit Windows the long is 4 bytes, but pointer is 8 bytes
     typedef uint64_t uintptr_t;
@@ -37,7 +30,6 @@ typedef signed long long int64_t;
     typedef unsigned long uintptr_t;
 #endif
 
-// Size types
 #if defined(_WIN32) && defined(_WIN64)
     // 64 bit Windows has 8 byte size_t (but 4 byte long)
     typedef uint64_t size_t;
@@ -49,64 +41,37 @@ typedef signed long long int64_t;
     typedef signed long ssize_t;
 #endif
 
-// NULL pointer
-#ifndef NULL
-#define NULL ((void*)0)
+#else
+// For SDL builds, ensure we have ssize_t if not provided by system
+#if !defined(_SSIZE_T_DEFINED) && !defined(_SSIZE_T_) && !defined(__ssize_t_defined)
+typedef long ssize_t;
 #endif
+#endif
+
+// NULL, boolean types, and limits (only for nostdlib builds)
+#if !defined(WASI_LINUX_SKIP_ENTRY) && !defined(WASI_MACOS_SKIP_ENTRY) && !defined(WASI_WINDOWS_SKIP_ENTRY)
+
+#define NULL ((void*)0)
 
 // Boolean type and constants
-#ifndef bool
 #define bool _Bool
-#endif
-#ifndef true
 #define true 1
-#endif
-#ifndef false
 #define false 0
-#endif
-#ifndef SIZE_MAX
 #define SIZE_MAX ((size_t)-1)
-#endif
 
-#ifndef INT8_C
 #define INT8_C(value) value
-#endif
-#ifndef UINT8_C
 #define UINT8_C(value) value##u
-#endif
-#ifndef INT16_C
 #define INT16_C(value) value
-#endif
-#ifndef UINT16_C
 #define UINT16_C(value) value##u
-#endif
-#ifndef INT32_C
 #define INT32_C(value) value
-#endif
-#ifndef UINT32_C
 #define UINT32_C(value) value##u
-#endif
-#ifndef INT64_C
 #define INT64_C(value) value##ll
-#endif
-#ifndef UINT64_C
 #define UINT64_C(value) value##ull
-#endif
-#ifndef INTMAX_C
 #define INTMAX_C(value) INT64_C(value)
-#endif
-#ifndef UINTMAX_C
 #define UINTMAX_C(value) UINT64_C(value)
-#endif
-#ifndef INT32_MAX
 #define INT32_MAX ((int32_t)0x7FFFFFFF)
-#endif
-#ifndef UINT32_MAX
 #define UINT32_MAX ((uint32_t)0xFFFFFFFFu)
-#endif
-#ifndef INT64_MAX
 #define INT64_MAX ((int64_t)0x7FFFFFFFFFFFFFFFll)
-#endif
-#ifndef UINT64_MAX
 #define UINT64_MAX ((uint64_t)0xFFFFFFFFFFFFFFFFull)
+
 #endif
