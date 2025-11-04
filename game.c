6bb50c0a6e6836cb837fffcba23f5c73baf5a339
Ondřej Čertík <ondrej@certik.us>
2025-11-04 02:53:09 -0700

    X

--- game.c
+++ game.c
@@ -1593,7 +1593,7 @@ static int complete_gpu_setup(GameApp *app) {
     }
 
     SDL_GPUTransferBufferCreateInfo transfer_info = {
-        .usage = SDL_GPU_BUFFERUSAGE_VERTEX,
+        .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
         .size = vertex_buffer_info.size,
     };
     app->scene_vertex_transfer_buffer = SDL_CreateGPUTransferBuffer(app->device, &transfer_info);
@@ -1643,7 +1643,7 @@ static int complete_gpu_setup(GameApp *app) {
     SDL_SubmitGPUCommandBuffer(upload_cmdbuf);
 
     SDL_GPUTransferBufferCreateInfo overlay_transfer_info = {
-        .usage = SDL_GPU_BUFFERUSAGE_VERTEX,
+        .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
         .size = sizeof(OverlayVertex) * MAX_OVERLAY_VERTICES,
     };
     app->overlay_transfer_buffer = SDL_CreateGPUTransferBuffer(app->device, &overlay_transfer_info);
