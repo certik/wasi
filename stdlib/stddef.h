efe7edca4e6df1e55649015d289145c21891c10a
Ondřej Čertík <ondrej@certik.us>
2025-11-04 00:49:55 -0700

    Fix

--- stdlib/stddef.h
+++ stdlib/stddef.h
@@ -2,4 +2,8 @@
 
 #include <base_types.h>
 
+#ifndef offsetof
+#define offsetof(type, member) __builtin_offsetof(type, member)
+#endif
+
 // Re-export types from base_types.h for stdlib compatibility
