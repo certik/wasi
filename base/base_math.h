ab8d8561009767cd405d2692d4b8faf8940314af
Ondřej Čertík <ondrej@certik.us>
2025-11-19 23:11:15 -0700

    Windows fix

--- base/base_math.h
+++ base/base_math.h
@@ -3,15 +3,34 @@
 #include <base/base_types.h>
 
 #ifndef NAN
+#ifdef _MSC_VER
+#define NAN ((float)(INFINITY * 0.0f))
+#else
 #define NAN (__builtin_nanf(""))
+#endif
 #endif
 
 #ifndef INFINITY
+#ifdef _MSC_VER
+// Use the standard C99 macro from math.h or define it directly
+#include <math.h>
+#ifndef INFINITY
+#define INFINITY ((float)(1e308 * 1e308))
+#endif
+#else
 #define INFINITY (__builtin_inff())
+#endif
 #endif
 
 #ifndef HUGE_VAL
+#ifdef _MSC_VER
+#include <math.h>
+#ifndef HUGE_VAL
+#define HUGE_VAL ((double)INFINITY)
+#endif
+#else
 #define HUGE_VAL (__builtin_huge_val())
+#endif
 #endif
 
 static inline double base_fabs(double x) {
