69aff25f3e19234577cb8874e59eb912152591f0
Ondřej Čertík <ondrej@certik.us>
2025-11-19 19:13:15 -0700

    Declare types

--- base/base_types.h
+++ base/base_types.h
@@ -32,10 +32,12 @@ typedef signed long long int64_t;
 #if defined(_WIN32) && defined(_WIN64)
     // For 64 bit Windows the long is 4 bytes, but pointer is 8 bytes
     typedef uint64_t uintptr_t;
+    typedef uint64_t ptrdiff_t;
 #else
     // For 32 bit platforms and wasm64 the long and a pointer is 4 bytes, for
     // 64 bit macOS/Linux the long and pointer is 8 bytes
     typedef unsigned long uintptr_t;
+    typedef unsigned long ptrdiff_t;
 #endif
 
 #if defined(_WIN32) && defined(_WIN64)
